a05d0bf15780a2601160279388f42d55e20bb957
eeriley99 <eriley@greenwichacademy.org>
2020-12-08 19:10:15 -0500

    updates

--- CreativeCoding/Day3Random/sketch.js
+++ CreativeCoding/Day3Random/sketch.js
@@ -1,32 +1,15 @@
 //3 Random
 
 function setup() {
-<<<<<<< Updated upstream
-  createCanvas(720, 720);
-  noFill();
-  noStroke();
-  frameRate(1);
-=======
     createCanvas(720, 720);
     noFill();
     noStroke();
     frameRate(1);
->>>>>>> Stashed changes
 }
 
 function draw() {
     background(255);
 
-<<<<<<< Updated upstream
-  var windowPane = 10; // padding around box array
-  var num = 7; //number of boxes in array
-  var shift = random(-5, 5); //degree of shift in quad
-  var space = 60; //space between squares
-
-  translate(windowPane + space / 2, windowPane + space / 2); //(x,y) start point offset for padding
-
-  var sideLen = (720 - 2 * windowPane) / num //length of individual side of a box
-=======
     var windowPane = 10; // padding around box array
     var num = 7; //number of boxes in array
     var shift = random(-5, 5); //degree of shift in quad
@@ -38,29 +21,9 @@ function draw() {
 
     for (var x = 0; x < num * sideLen; x = x + sideLen) {
         for (var y = 0; y < num * sideLen; y = y + sideLen) {
->>>>>>> Stashed changes
 
             fill(random(200, 255), 0, random(200, 255));
 
-<<<<<<< Updated upstream
-      fill(random(200, 255), 0, random(200, 255));
-      
-      //a random shift is added to each vertex
-      // add length of individual side of a box (num) and 
-      //subtract space between squares (space) to keep spacing true
-      
-      quad(random(-shift, shift) + x, 
-           random(-shift, shift) + y, 
-           
-           random(-shift, shift) + x + sideLen - space, 
-           random(-shift, shift) + y, 
-           
-           random(-shift, shift) + x + sideLen - space, 
-           random(-shift, shift) + y + sideLen - space, 
-           
-           random(-shift, shift) + x, 
-           random(-shift, shift) + y + sideLen - space);
-=======
             //a random shift is added to each vertex
             // add length of individual side of a box (num) and 
             //subtract space between squares (space) to keep spacing true
@@ -77,6 +40,5 @@ function draw() {
                 random(-shift, shift) + x,
                 random(-shift, shift) + y + sideLen - space);
         }
->>>>>>> Stashed changes
     }
 }
