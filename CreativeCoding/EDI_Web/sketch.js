854339222b60427200cf326a128e6d20257cae99
eeriley99 <eriley@greenwichacademy.org>
2020-12-08 23:02:24 -0500

    updates

--- CreativeCoding/EDI_Web/sketch.js
+++ CreativeCoding/EDI_Web/sketch.js
@@ -5,7 +5,7 @@ function setup() {
     noFill();
     noStroke();
     frameRate(1);
-    cursor('https://eeriley99.github.io/code_sketchbook/icons-master/clickFinger.gif');
+    cursor('https://eeriley99.github.io/code_sketchbook/icons-master/sparkle.gif');
 }
 
 function draw() {
