972f6b1ab053cad13b33790a470854e2d0278944
eeriley99 <eriley@greenwichacademy.org>
2020-12-09 10:19:57 -0500

    spray can upload

--- CreativeCoding/EDI_Web/sketch.js
+++ CreativeCoding/EDI_Web/sketch.js
@@ -9,11 +9,13 @@ function setup() {
 }
 
 function draw() {
-    background(255);
+    clear(),
+
+        background(random(200, 255), random(200, 255), random(200, 255), 100);
 
     var windowPane = 10; // padding around box array
     var num = 4; //number of boxes in array
-    var shift = random(-5, 5); //degree of shift in quad
+    var shift = random(-7, 7); //degree of shift in quad
     var space = 100; //space between squares
 
     translate(windowPane + space / 2, windowPane + space / 2); //(x,y) start point offset for padding
@@ -23,7 +25,7 @@ function draw() {
     for (var x = 0; x < num * sideLen; x = x + sideLen) {
         for (var y = 0; y < num * sideLen; y = y + sideLen) {
 
-            fill(random(200, 255), 0, random(200, 255));
+            fill(random(200, 255), random(200, 255), random(200, 255), 100);
 
             //a random shift is added to each vertex
             // add length of individual side of a box (num) and 
---
+++ CreativeCoding/Spray_can/sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+  createCanvas(750, 750);
+  cursor('https://eeriley99.github.io/code_sketchbook/icons-master/spray_can.gif');
+}
+
+function draw (){
+  background (200);
+  
+}
