c38e831fd845308d2b0cca5c7db6d7f956250164
eeriley99 <eriley@greenwichacademy.org>
2020-12-09 10:40:35 -0500

    delete folder

--- CreativeCoding/Spray_can/sketch.js
+++
@@ -1,9 +0,0 @@
-function setup() {
-  createCanvas(750, 750);
-  cursor('https://eeriley99.github.io/code_sketchbook/icons-master/spray_can.gif');
-}
-
-function draw (){
-  background (200);
-  
-}
