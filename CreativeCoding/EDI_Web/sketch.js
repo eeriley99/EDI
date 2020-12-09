5f8ba7d5137178477096eabe54af35a70bbc4ce4
eeriley99 <eriley@greenwichacademy.org>
2020-12-09 11:15:41 -0500

    Update sketch.js

--- CreativeCoding/EDI_Web/sketch.js
+++ CreativeCoding/EDI_Web/sketch.js
@@ -5,7 +5,7 @@ function setup() {
     noFill();
     noStroke();
     frameRate(1);
-    cursor('https://eeriley99.github.io/code_sketchbook/icons-master/rnbw.gif');
+    cursor('https://eeriley99.github.io/code_sketchbook/icons-master/compIcon.gif');
 }
 
 function draw() {
