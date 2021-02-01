dc06afdd436b00be806722036899d8250e26a2bb
eeriley99 <eriley@greenwichacademy.org>
2021-02-01 11:57:51 -0500

    update

---
+++ CreativeCoding/OlliesBG/sketch.js
@@ -0,0 +1,41 @@
+let img;
+
+function preload() {
+    img = loadImage('/Images/DayWindow3.png');
+}
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    noStroke();
+}
+
+function draw() {
+
+    var num = 4;
+
+    var XsideLen = windowWidth / num;
+
+    // var YsideLen = windowHeight / num);
+
+
+    for (var dy = 0; dy < windowHeight; dy = dy + XsideLen * 0.53) {
+
+        for (var dx = 0; dx < windowWidth; dx = dx + XsideLen) {
+
+            quad(dx, dy,
+                dx + XsideLen, dy,
+                dx + XsideLen, dy + XsideLen * 0.53,
+                dx, dy + XsideLen * 0.53);
+
+            image(img, dx, dy, XsideLen, XsideLen * 0.53);
+        }
+
+
+    }
+    print(XsideLen);
+    print(XsideLen * 0.53);
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
