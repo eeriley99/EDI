f11c24acf505935e25b3bbbfa1dd6eaee67c6f95
eeriley99 <eriley@greenwichacademy.org>
2021-02-07 00:16:52 -0500

    update images

--- CreativeCoding/randImgLink/topSketch.js
+++ CreativeCoding/randImgLink/topSketch.js
@@ -1,8 +1,10 @@
 function setup() {
     createCanvas(innerWidth, innerHeight);
+    eRiley = loadImage('assets/eriley.png');
     port = loadImage('assets/portfolio.png');
     resume = loadImage('assets/Resume.png');
     writing = loadImage('assets/writing.png');
+    sWork = loadImage('assets/swork.png');
     topBuffer = createGraphics(innerWidth, innerHeight);
     noStroke();
     noFill();
@@ -13,36 +15,53 @@ function draw() {
 }
 
 function changeCursor() {
-    if (mouseY < innerHeight / 2 && mouseX < innerWidth / 2) {
-        cursor('https://eeriley99.github.io/code_sketchbook/icons-master/cans.png');
-        image(resume, mouseX, mouseY);
-        tint(255, 10);
+    if (mouseY < innerHeight / 16) {
+        image(eRiley, mouseX, mouseY);
+        tint(255, 5);
     }
-    if (mouseY < innerHeight / 2 && mouseX > innerWidth / 2) {
+
+    if (mouseY < innerHeight / 2 && mouseY < innerHeight / 16 && mouseX < innerWidth / 2) {
+        cursor('https://eeriley99.github.io/code_sketchbook/icons-master/shirt.png');
+        image(resume, mouseX, mouseY);
+        tint(255, 5);
+    }
+    if (mouseY < innerHeight / 2 && mouseY < innerHeight / 16 && mouseX > innerWidth / 2) {
         cursor('https://eeriley99.github.io/code_sketchbook/icons-master/hand.png');
         image(port, mouseX, mouseY);
-        tint(255, 10);
+        tint(255, 5);
     }
-    if (mouseY > innerHeight / 2) {
+    if (mouseY > innerHeight / 2 && mouseX < innerWidth / 2) {
         cursor('https://eeriley99.github.io/code_sketchbook/icons-master/feather.png');
         image(writing, mouseX, mouseY);
-        tint(255, 10);
+        tint(255, 5);
+    }
+    if (mouseY > innerHeight / 2 && mouseX > innerWidth / 2) {
+        cursor('https://eeriley99.github.io/code_sketchbook/icons-master/sWork.png');
+        image(sWork, mouseX, mouseY);
+        tint(255, 5);
     }
 }
 
 function mousePressed() {
-    if (mouseY < innerHeight / 2 && mouseX < innerWidth / 2) {
+
+
+    if (mouseY < innerHeight / 2 && mouseY < innerHeight / 16 && mouseX < innerWidth / 2) {
         window.open("https://eeriley99.github.io/EDI/CreativeCoding/OM-E-Dv2/ResumeSample.html", target = "_blank");
     }
 
-    if (mouseY < innerHeight / 2 && mouseX > innerWidth / 2) {
+    if (mouseY < innerHeight / 2 && mouseY < innerHeight / 16 && mouseX > innerWidth / 2) {
         //range accounting for text length
-        window.open("https://erinriley.weebly.com/", target = "_blank");
+        window.open("https://erinerileyart-blog-blog.tumblr.com/", target = "_blank");
     }
 
-    if (mouseY > innerHeight / 2) {
+    if (mouseY > innerHeight / 2 && mouseX < innerWidth / 2) {
         //range accounting for text length
         window.open("http://www.artofdigitalfabrication.com/", target = "_blank");
+    }
+
+    if (mouseY > innerHeight / 2 && mouseX > innerWidth / 2) {
+        //range accounting for text length
+        window.open("https://erinriley.weebly.com/", target = "_blank");
     }
 }
 
